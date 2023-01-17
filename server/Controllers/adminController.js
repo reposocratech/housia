6297501640bcfd7190e725b0b9c0e8cfe5bf4103
marcos-benitez <marcos.benitez.malaga@gmail.com>
2023-01-17 14:18:36 +0100

    CRUD features

--- server/Controllers/adminController.js
+++ server/Controllers/adminController.js
@@ -60,6 +60,252 @@ class adminController {
         });
     }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// CONSEGUIR LA INFO DE TODOS LOS FEATURES EXISTENTES
+
+    getPropertyFeatures = (req, res) =>{
+        let sql ="SELECT * FROM feature";
+        connection.query(sql, (error, result)=>{
+            if (error){
+                res.status(400).json({error});
+                
+            }
+            res.status(200).json(result);
+            console.log(result);
+        });
+    };
+
+
+//CREAR UN NUEVO FEATURE NO EXISTENTE
+
+    createPropertyFeatures = (req, res) => {
+        let {feature_name} = req.body;
+
+        let sql =`INSERT INTO type (type_name) VALUES (${feature_name})`;
+
+        connection.query(sql, (error, result)=>{
+            if (error){
+                res.status(400).json({error});
+                
+            }
+            res.status(200).json(result);
+            
+        });
+    }
+
+
+    //EDITAR UN FEATURE YA EXISTENTE
+
+    editPropertyFeature = (req, res) =>{
+        let {feature_id} = req.params;
+        let {feature_name} = req.body;
+
+        let sql =`UPDATE type SET type_name = ${feature_name} where type_id = ${feature_id}`;
+
+        connection.query(sql, (error, result)=>{
+            if (error){
+                res.status(400).json({error});
+                
+            }
+            res.status(200).json(result);
+            console.log(result);
+        });
+
+    }
+
+
+
+// BORRAR UN FEATURE YA EXISTENTE.
+
+    deletePropertyFeature = (req, res) =>{
+        let {feature_id} = req.params
+        console.log(req.params.feature_id);
+
+
+        let sql =`DELETE FROM feature where feature_id = ${feature_id}`;
+        console.log(sql);
+
+        connection.query(sql, (error, result)=>{
+            if (error){
+                res.status(400).json({error});
+                
+            }
+            res.status(200).json(result);
+            console.log(result);
+        });
+    }
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 module.exports = new adminController();
--- server/routes/admin.js
+++ server/routes/admin.js
@@ -16,4 +16,55 @@ router.put("/editPropertyType/:type_id", adminController.editPropertyType);
 //localhost:4000/admin/deletePropertyType/:type_id
 router.delete("/deletePropertyType/:type_id", adminController.deletePropertyType);
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+////features
+//localhost:4000/admin/getPropertyFeatures
+router.get("/getPropertyFeatures", adminController.getPropertyFeatures
+);
+
+//localhost:4000/admin/createPropertyFeatures
+router.post("/createPropertyFeatures", adminController.createPropertyFeatures);
+
+//localhost:4000/admin/editPropertyFeature/:feature_id
+router.put("/editPropertyFeature/:feature_id", adminController.editPropertyFeature);
+
+//localhost:4000/admin/deletePropertyFeature/:feature_id
+router.delete("/deletePropertyFeature/:feature_id", adminController.deletePropertyFeature);
+
+
 module.exports = router;
