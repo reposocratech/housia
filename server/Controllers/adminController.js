9ebcdeb89436cb5ad90be9f065b10773a5a8c33d
Marianela Serodino Runco <serodinonela@gmail.com>
2023-01-17 13:07:08 +0000

    CRUD kitchen y sistema de carpetas

--- server/Controllers/adminController.js
+++ server/Controllers/adminController.js
@@ -2,6 +2,8 @@ const connection = require("../config/db.js");
 
 
 class adminController {
+    // CRUD Property
+    //Trae los tipos de propiedades
     getPropertyTypes = (req, res) =>{
         let sql ="SELECT * FROM type "
         connection.query(sql, (error, result)=>{
@@ -14,6 +16,7 @@ class adminController {
         });
     };
 
+    //crear un tipo de propiedad
     createPropertyType = (req, res) => {
         let {type_name} = req.body;
 
@@ -29,6 +32,7 @@ class adminController {
         });
     }
 
+    //edita una propiedad
     editPropertyType = (req, res) => {
         let {type_id} = req.params;
         let {type_name} = req.body;
@@ -45,10 +49,76 @@ class adminController {
         });
     }
     
+    //borra una propiedad
     deletePropertyType = (req, res) => {
         let {type_id} = req.params;
 
         let sql =`DELETE FROM type where type_id = ${type_id}`;
+
+        connection.query(sql, (error, result)=>{
+            if (error){
+                res.status(400).json({error});
+                
+            }
+            res.status(200).json(result);
+            console.log(result);
+        });
+    }
+
+    // CRUD Kitchen
+
+    //trae la info de todos los tipos de cocina
+    getKitchenTypes = (req, res) =>{
+        let sql ="SELECT * FROM kitchen "
+        connection.query(sql, (error, result)=>{
+            if (error){
+                res.status(400).json({error});
+                
+            }
+            res.status(200).json(result);
+            console.log(result);
+        });
+    };
+
+    //crea un tipo de cocina
+    createKitchenType = (req, res) => {
+        let {kitchen_name} = req.body;
+
+        let sql =`INSERT INTO kitchen (kitchen_name) VALUES (${kitchen_name})`;
+        console.log(sql);
+
+        connection.query(sql, (error, result)=>{
+            if (error){
+                res.status(400).json({error});
+                
+            }
+            res.status(200).json(result);
+            console.log(result);
+        });
+    }
+
+    //edita un tipo de cocina
+    editKitchenType = (req, res) => {
+        let {kitchen_id} = req.params;
+        let {kitchen_name} = req.body;
+
+        let sql =`UPDATE kitchen SET kitchen_name = ${kitchen_name} where kitchen_id = ${kitchen_id}`;
+
+        connection.query(sql, (error, result)=>{
+            if (error){
+                res.status(400).json({error});
+                
+            }
+            res.status(200).json(result);
+            console.log(result);
+        });
+    }
+    
+    //borra un tipo de cocina
+    deleteKitchenType = (req, res) => {
+        let {kitchen_id} = req.params;
+
+        let sql =`DELETE FROM kitchen where kitchen_id = ${kitchen_id}`;
 
         connection.query(sql, (error, result)=>{
             if (error){
--- server/routes/admin.js
+++ server/routes/admin.js
@@ -4,6 +4,7 @@ var adminController = require('../Controllers/adminController')
 
 // localhost:4000/admin
 
+// Property types
 //localhost:4000/admin/getPropertyTypes
 router.get("/getPropertyTypes", adminController.getPropertyTypes);
 
@@ -15,5 +16,20 @@ router.put("/editPropertyType/:type_id", adminController.editPropertyType);
 
 //localhost:4000/admin/deletePropertyType/:type_id
 router.delete("/deletePropertyType/:type_id", adminController.deletePropertyType);
+
+
+
+// Kitchen types
+//localhost:4000/admin/getKitchenTypes
+router.get("/getKitchenTypes", adminController.getKitchenTypes);
+
+//localhost:4000/admin/createKitchenType
+router.post("/createKitchenType", adminController.createKitchenType);
+
+//localhost:4000/admin/editKitchenType/:kitchen_id
+router.put("/editKitchenType/:kitchen_id", adminController.editKitchenType);
+
+//localhost:4000/admin/deleteKitchenType/:kitchen_id
+router.delete("/deleteKitchenType/:kitchen_id", adminController.deleteKitchenType);
 
 module.exports = router;
