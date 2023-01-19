e428955e1bba93fd262f3e3f5c9cbff7c88727fe
JoseCasta23 <joseantoniocastanedarios23@gmail.com>
2023-01-19 13:15:04 +0100

    eliminamos my-app

--- client/src/Routes/AppRoutes.js
+++ client/src/Routes/AppRoutes.js
@@ -4,6 +4,8 @@ import { Home } from '../Pages/UserDashboard/Home/Home'
 import { Login } from '../Pages/Auth/Login/Login'
 import { Register } from '../Pages/Auth/Register/Register'
 import { NavBarUser } from '../Components/NavbarUser/NavBarUser'
+import { AddPropertyForm1 } from '../Components/AddPropertyForm/AddPropertyForm1'
+import { AddProperty2 } from '../Components/AddPropertyForm/AddProperty2'
 
 export const AppRoutes = () => {
   return (
@@ -14,6 +16,8 @@ export const AppRoutes = () => {
                 <Route path='/' element={<Home/>}/>
                 <Route path='/register' element={<Register/>}/>
                 <Route path='/login' element={<Login/>}/>
+                <Route path='/addProperty' element={<AddPropertyForm1/>}/>
+                <Route path='/addProperty2' element={<AddProperty2/>}/>
             </Routes>
         </BrowserRouter>
     </div>
