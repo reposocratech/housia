b915607c129f73373bef6ea7dc3f46d8fd14bbe8
marcos-benitez <marcos.benitez.malaga@gmail.com>
2023-01-31 15:21:25 +0100

    favoritos funcionando y resumen css done

--- client/src/Pages/property/SummaryInversionUser.js
+++ client/src/Pages/property/SummaryInversionUser.js
@@ -14,7 +14,7 @@ export const SummaryInversionUser = () => {
 
     const {user} = useContext(AppContext);
     const {user_id} = user;
-    console.log(user_id, "****************");
+    console.log(user_id, "USER_ID");
     
     useEffect(() => {
         axios 
@@ -85,67 +85,69 @@ export const SummaryInversionUser = () => {
  
     
   return (
-    <Container>
+    <Container fluid className='main' >
         <h2 className='text-center'>RESUMEN INVERSIÓN</h2>
        
-        <Row>
-            <Col>
+        <Row className='m-0'>
+            
+            <Col className=' profit' xs={12} md={12} lg={4}>
                 <h3>Beneficio</h3>
-                <p className='fs-1'>322220€</p>
+                <p className='fs-1'>{Math.floor(totalInv * 0.15)} €</p>
             </Col>
-            <Col>
-                <img src='/images/valoracion_patrimonio.png' alt='valoracion_patrimonio'/>
+        
+            <Col className=' graphic ' xs={12} md={12} lg={8}>
+                <img src='/images/property/grafica.jpg' alt='valoracion_patrimonio'/>
             </Col>
-        </Row>
-        <Row className='mt-5'>
-            <Col>
+        </Row >
+        <Row className='datos justify-content-evenly'>
+            <Col className='m-4 info'  xs={12} md={6} lg={3}>
                 <h3>Información</h3>
-                <div className='d-flex justify-content-between border border-dark rounded-4 p-2' >
+                <div className='d-flex justify-content-around ' >
                     <span>Total P/L</span> 
-                    <span>312000€</span> 
-                    <span> -4%</span>
+                    <span>{Math.floor(totalInv * 0.03)} €</span> 
+                    <span> 3 %</span>
                 </div>
 
-                <div className='d-flex justify-content-between border border-dark rounded-4 p-2 mt-3'>
+                <div className='d-flex justify-content-around border border-dark rounded-4 p-2 mt-3'>
                     <span>Inversión Total</span> 
-                    <span>{totalInv}€ </span> 
-                    <span> -4%</span>
+                    <span>{totalInv} € </span> 
+                    <span> -2.8 %</span>
                 </div>
 
-                <div className='d-flex justify-content-between border border-dark rounded-4 p-2 mt-3'>
+                <div className='d-flex justify-content-around border border-dark rounded-4 p-2 mt-3'>
                     <span>Gastos mensuales</span> 
-                    <span>1400€</span> 
-                    <span> -4%</span>
+                    <span>1400 €</span> 
+                    <span> -4 %</span>
                 </div>
 
-                <div className='d-flex justify-content-between border border-dark rounded-4 p-2 mt-3'>
+                <div className='d-flex justify-content-around border border-dark rounded-4 p-2 mt-3'>
                     <span>Ingresos Menusales</span> 
-                    <span>{monthlyInc}€ </span> 
-                    <span> -5%</span>
+                    <span>{monthlyInc} € </span> 
+                    <span> 0 %</span>
                 </div>
             </Col>
-            <Col >
+            <Col className='m-4 medio' xs={12} md={6} lg={3}>
                 <h3>Histórico Inmueble</h3>
-                <div className='border border-dark rounded-4 p-3'>
-                    <div className='d-flex justify-content-between'>
+                <div className='border border-dark rounded-4'>
+                    <div className='d-flex justify-content-between tiposMedio'>
                         <span>Inmuebles activos: </span>
                         <span>{countProperties}</span>
                     </div>
-                    <div className='d-flex justify-content-between'>
+                    <div className='d-flex justify-content-between tiposMedio'>
                         <span>Operaciones cerradas: </span>
                         <span>{soldProperties}</span>
                     </div>
-                    <div className='d-flex justify-content-between'>
+                    <div className='d-flex justify-content-between tiposMedio'>
                         <span>Inmuebles en alquiler: </span>
                         <span>{rentedProperties}</span>
                     </div>
-                    <div className='d-flex justify-content-between'>
+                    <div className='d-flex justify-content-between tiposMedio'>
                         <span>Rendimiento medio: </span>
                         <span>12%</span>
                     </div>
                 </div>
             </Col>
-            <Col><h3>Récords de Inversión</h3></Col>
+            <Col className='m-4 record control' xs={12} md={6} lg={3}><h3>Récords de Inversión</h3></Col>
         </Row>
 
 
